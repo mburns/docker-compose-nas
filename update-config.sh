f624da0bb21011e3be0fefb87a148e3c38294b32
Michael Burns <maburns@gmail.com>
2023-11-01 18:54:29 -0700

    add plex, etc

--- update-config.sh
+++ update-config.sh
@@ -34,12 +34,42 @@ done
 sed -i.bak "s/<UrlBase><\/UrlBase>/<UrlBase>\/prowlarr<\/UrlBase>/" ./prowlarr/config.xml && rm ./prowlarr/config.xml.bak
 sed -i.bak 's/^PROWLARR_API_KEY=.*/PROWLARR_API_KEY='"$(sed -n 's/.*<ApiKey>\(.*\)<\/ApiKey>.*/\1/p' ./prowlarr/config.xml)"'/' .env && rm .env.bak
 
-echo "Updating Jellyfin configuration..."
-until [ -f ./jellyfin/network.xml ]
+echo "Updating Kapowarr configuration..."
+until [ -f ./kapowarr/config.xml ]
 do
   sleep 5
 done
-sed -i.bak "s/<BaseUrl \/>/<BaseUrl>\/jellyfin<\/BaseUrl>/" ./jellyfin/network.xml && rm ./jellyfin/network.xml.bak
+sed -i.bak "s/<UrlBase><\/UrlBase>/<UrlBase>\/kapowarr<\/UrlBase>/" ./kapowarr/config.xml && rm ./kapowarr/config.xml.bak
+sed -i.bak 's/^KAPOWARR_API_KEY=.*/KAPOWARR_API_KEY='"$(sed -n 's/.*<ApiKey>\(.*\)<\/ApiKey>.*/\1/p' ./kapowarr/config.xml)"'/' .env && rm .env.bak
+
+echo "Updating Readarr configuration..."
+until [ -f ./readarr/config.xml ]
+do
+  sleep 5
+done
+sed -i.bak "s/<UrlBase><\/UrlBase>/<UrlBase>\/readarr<\/UrlBase>/" ./readarr/config.xml && rm ./readarr/config.xml.bak
+sed -i.bak 's/^READARR_API_KEY=.*/READARR_API_KEY='"$(sed -n 's/.*<ApiKey>\(.*\)<\/ApiKey>.*/\1/p' ./readarr/config.xml)"'/' .env && rm .env.bak
+
+# overseerr
+# plex
+# tautulli
+
+# qbittorrent
+
+echo "Updating Plex configuration..."
+until [ -f ./plex/config.ini ]
+do
+  sleep 5
+done
+sed -i.bak "s/<BaseUrl \/>/<BaseUrl>\/plex<\/BaseUrl>/" ./plex/network.xml && rm ./plex/network.xml.bak
+
+
+# echo "Updating Jellyfin configuration..."
+# until [ -f ./jellyfin/network.xml ]
+# do
+#   sleep 5
+# done
+# sed -i.bak "s/<BaseUrl \/>/<BaseUrl>\/jellyfin<\/BaseUrl>/" ./jellyfin/network.xml && rm ./jellyfin/network.xml.bak
 
 echo "Restarting containers..."
-docker compose restart radarr sonarr prowlarr jellyfin
+docker compose restart radarr sonarr prowlarr kapowarr readarr
